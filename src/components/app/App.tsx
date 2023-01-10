f6b3773be6b25f79e80db6316fd6302581574635
omolayo clement <naijawebmaster1@gmail.com>
2023-01-10 19:20:08 +0100

    first commit

---
+++ src/components/app/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1 className="text-9xl">Hello world!</h1>
+			</header>
+		</div>
+	);
+}
+
+export default App;
